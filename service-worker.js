7f38422e242d5e0b9f39ebe248e5fb7d99b02f47
KishanBali <kishansaraiya9@gmail.com>
2026-09-15 14:06:00 +0530

    Refresh PWA cache so latest Install KEMP EYE card is served

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kemp-cymatics-pwa-v1';
+const CACHE='kemp-cymatics-pwa-v2';
 const CORE=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
